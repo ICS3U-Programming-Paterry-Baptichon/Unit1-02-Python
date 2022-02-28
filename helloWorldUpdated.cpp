cc7366d56509cc30394f09899a54bb463a4d131d
Ubuntu <ubuntu@ip-172-31-55-224.ec2.internal>
2022-02-28 13:49:19 +0000

    bgyuuhfg5r

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,12 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Junior";
+  cout << "Please enter an integer value: ";
+  cin >> i;
+  cout << "The value you entered is " << i;
+  cout << " and its double is " << i*2 << ".\n";
+  return 0;
+}
